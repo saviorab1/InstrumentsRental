63fd1badc48a189a47692e376e7ac2b3ac4c82bc
saviorab1 <popww147@gmail.com>
2025-03-10 09:56:28 +0700

    Fix Parcel to also inherit the term of lease

--- app/src/main/java/com/example/instrumentsrental/model/RentalDetails.kt
+++ app/src/main/java/com/example/instrumentsrental/model/RentalDetails.kt
@@ -1,7 +1,7 @@
 package com.example.instrumentsrental.model
 
+import android.os.Parcel
 import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
 
 /**
  * RentalDetails - Data class representing details of an instrument rental.
@@ -15,7 +15,6 @@ import kotlinx.parcelize.Parcelize
  * @property isMonthly Whether the rental period is in months (true) or weeks (false)
  * @property quantity Number of weeks/months for the rental period
  */
-@Parcelize
 data class RentalDetails(
     val instrumentName: String,
     val instrumentDescription: String,
@@ -24,4 +23,45 @@ data class RentalDetails(
     val totalPrice: Int,
     val isMonthly: Boolean,
     val quantity: Int
-) : Parcelable 
+) : Parcelable {
+    
+    // Constructor that takes a Parcel and reads the data in the same order it was written
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readInt(),
+        parcel.readString() ?: "",
+        parcel.readInt(),
+        parcel.readByte() != 0.toByte(),  // Convert byte to boolean
+        parcel.readInt()
+    )
+    
+    // Write object's data to the parcel
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(instrumentName)
+        parcel.writeString(instrumentDescription)
+        parcel.writeInt(instrumentImageRes)
+        parcel.writeString(rentalPeriod)
+        parcel.writeInt(totalPrice)
+        parcel.writeByte(if (isMonthly) 1 else 0)  // Convert boolean to byte
+        parcel.writeInt(quantity)
+    }
+    
+    // Return a bitmask indicating the set of special object types in this Parcelable
+    override fun describeContents(): Int {
+        return 0
+    }
+    
+    // Static CREATOR field required for Parcelable implementation
+    companion object CREATOR : Parcelable.Creator<RentalDetails> {
+        // Create a new instance of the Parcelable class, from a Parcel
+        override fun createFromParcel(parcel: Parcel): RentalDetails {
+            return RentalDetails(parcel)
+        }
+        
+        // Create a new array of the Parcelable class
+        override fun newArray(size: Int): Array<RentalDetails?> {
+            return arrayOfNulls(size)
+        }
+    }
+} 
